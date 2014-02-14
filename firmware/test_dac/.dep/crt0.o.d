3fdef3b6c929bda6f4900c6d19fb862211df1b99
z80 <bashkirov.sergey@gmail.com>
2014-02-14 14:07:09 +0400

    Added chibios and projects.

---
+++ firmware/test_dac/.dep/crt0.o.d
@@ -0,0 +1,7 @@
+build/obj/crt0.o: ../chibios_2.6.3/os/ports/GCC/ARMCMx/crt0.c \
+ c:\programs\codesourcery\arm-2012.03-56\bin\../lib/gcc/arm-none-eabi/4.5.2/include/stdint.h \
+ c:\programs\codesourcery\arm-2012.03-56\bin\../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/stdint.h
+
+c:\programs\codesourcery\arm-2012.03-56\bin\../lib/gcc/arm-none-eabi/4.5.2/include/stdint.h:
+
+c:\programs\codesourcery\arm-2012.03-56\bin\../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/stdint.h:
