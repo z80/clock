f8ee4a406860f940268016f39c06652476211c5c
z80 <bashkirov.sergey@gmail.com>
2014-03-19 09:38:58 +0400

    Trying to revert...

--- firmware/firmware/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-PATH=$PATH:~/programs/code-sourcery/2011.03-42/bin
-make
--- firmware/firmware/flash.sh
+++ firmware/firmware/flash.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+<<<<<<< HEAD
 ~/projects/stlink.git/stlink-texane/flash/st-flash write ./build/ch.bin 0x08000000
+=======
+../../st-link-gdb-server-linux/flash/st-flash write ./target/target.bin 0x08000000
+>>>>>>> parent of b886b3f... Just compiled old file.
 
