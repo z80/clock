339ffc3deb7e875d6c9187c9283b1606a109d661
z80 <bashkirov.sergey@gmail.com>
2014-03-19 09:39:42 +0400

    Revert "Just compiled old file."
    This reverts commit b886b3fa96096836dc1765796f663618c6a8a014.
    
    Conflicts:
    	firmware/firmware/debug_ext.sh
    	firmware/firmware/flash.sh

--- firmware/firmware/flash.sh
+++ firmware/firmware/flash.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 <<<<<<< HEAD
+<<<<<<< HEAD
 ~/projects/stlink.git/stlink-texane/flash/st-flash write ./build/ch.bin 0x08000000
+=======
+../../st-link-gdb-server-linux/flash/st-flash write ./target/target.bin 0x08000000
+>>>>>>> parent of b886b3f... Just compiled old file.
 =======
 ../../st-link-gdb-server-linux/flash/st-flash write ./target/target.bin 0x08000000
 >>>>>>> parent of b886b3f... Just compiled old file.
