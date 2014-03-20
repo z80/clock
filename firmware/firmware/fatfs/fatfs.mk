070adb7a6d5c13d530004738e91bfdd48adb7e6b
unknown <z80@cortex-m3.(none)>
2014-03-19 16:38:43 -0800

    Prepared project for SD card support adjustment.

---
+++ firmware/firmware/fatfs/fatfs.mk
@@ -0,0 +1,12 @@
+# FATFS files.
+FATFSSRC = fatfs/fatfs_diskio.c \
+           fatfs/fatfs_syscall.c \
+           fatfs/src/ff.c \
+           fatfs/src/option/ccsbcs.c
+
+FATFSINC = fatfs/src
+
+
+
+
+
